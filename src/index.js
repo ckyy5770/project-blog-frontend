e38bbedbac614d84276cd33dd912f3ba4f4ff592
iloveyoukcl5770 <iloveyoukcl5770@gmail.com>
2017-08-15 00:48:34 -0500

    deploy on heroku, change style import statements

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './antd.min.css';
+import './bootstrap.min.css';
+import './style.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
